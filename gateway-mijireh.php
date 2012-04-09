8b87794aa3e9cbc31766ed4d7576985440ee1dc0
Lee Blue <Lee@Reality66.com>
2012-04-09 18:44:32 -0400

    integrating page slurp into woocommerce extension

--- gateway-mijireh.php
+++ gateway-mijireh.php
@@ -12,15 +12,38 @@ Author URI: http://www.mijireh.com/
 	License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
+// Register activation hook to install page slurp page
+register_activation_hook(__FILE__, 'install_slurp_page');
+register_uninstall_hook(__FILE__, 'remove_slurp_page');
+
+function install_slurp_page() {
+  if(!get_page_by_path('mijireh-secure-checkout')) {
+    $page = array(
+      'post_title' => 'Mijireh Secure Checkout',
+      'post_name' => 'mijireh-secure-checkout',
+      'post_parent' => 0,
+      'post_status' => 'private',
+      'post_type' => 'page',
+      'comment_status' => 'closed',
+      'ping_status' => 'closed',
+      'post_content' => "<h1>Checkout</h1>\n\n{{mj-checkout-form}}",
+    );
+    wp_insert_post($page);
+  }
+}
+
+function remove_slurp_page() {
+  $force_delete = true;
+  $post = get_page_by_path('mijireh-secure-checkout');
+  wp_delete_post($post->ID, $force_delete);
+}
+
 add_action('plugins_loaded', 'woocommerce_mijireh_checkout_init', 0);
 
 function woocommerce_mijireh_checkout_init() {
 
 	if ( !class_exists( 'WC_Payment_Gateway' ) ) return;
-
-	/**
- 	 * Localisation
-	 */
+	
 	load_plugin_textdomain('wc-mijireh-checkout', false, dirname( plugin_basename( __FILE__ ) ) . '/languages');
     
 	/**
@@ -32,25 +55,37 @@ function woocommerce_mijireh_checkout_init() {
 	    global $woocommerce;
 	    
 	    if(!class_exists('Mijireh')) {
-	      require 'mijireh.php';
+	      require 'Mijireh.php';
 	    }
-
+	    
   		$this->id = 'mijireh_checkout';
   		$this->icon = apply_filters('mijireh_checkout_icon', WP_PLUGIN_URL . "/" . plugin_basename( dirname(__FILE__)) . '/images/mijireh.png');
       $this->has_fields = false;
-      $this->liveurl = 'https://secure.mijireh.com/api/1/';
       $this->method_title = __( 'Mijireh Checkout', 'mijireh_checkout' );
+      $this->url = WP_PLUGIN_URL . "/" . plugin_basename( dirname(__FILE__));
       
       $this->init_form_fields();
       $this->init_settings();
       
+      Mijireh::$access_key = $this->settings['access_key'];
       $this->title = $this->settings['title'];
   		$this->description = $this->settings['description'];
-  		$this->access_key = $this->settings['access_key'];
+  		
   		
 			add_action('init', array(&$this, 'mijireh_notification'));
   		add_action('woocommerce_update_options_payment_gateways', array(&$this, 'process_admin_options'));
-  		
+  		add_action('add_meta_boxes', array(&$this, 'add_page_slurp_meta'));
+  		add_action('wp_ajax_page_slurp', array(&$this, 'page_slurp'));
+	  }
+	  
+	  public function page_slurp() {
+	    $page = get_page($_POST['page_id']);
+	    $url = get_permalink($page->ID);
+      wp_update_post(array('ID' => $page->ID, 'post_status' => 'publish'));
+      $job_id = Mijireh::slurp($url);
+      wp_update_post(array('ID' => $page->ID, 'post_status' => 'private'));
+      echo $job_id;
+      die;
 	  }
 	  
 	  public function mijireh_notification() {
@@ -60,7 +95,6 @@ function woocommerce_mijireh_checkout_init() {
 	      unset($_SESSION['order_awaiting_payment']);
 	      
 	      try {
-	        Mijireh::$access_key = $this->access_key;
   	      $mj_order = new Mijireh_Order($_GET['order_number']);
   	      $wc_order_id = $mj_order->get_meta_value('wc_order_id');
   	      $wc_order = new WC_Order($wc_order_id);
@@ -106,7 +140,13 @@ function woocommerce_mijireh_checkout_init() {
 					'type' => 'text', 
 					'description' => __( 'This controls the title which the user sees during checkout.', 'mijireh_checkout' ), 
 					'default' => __( 'Mijireh Checkout', 'mijireh_checkout' )
-				)
+				),
+				'description' => array(
+  				'title' => __( 'Description', 'mijireh_checkout' ), 
+  				'type' => 'textarea', 
+  				'description' => __( 'Description of Mijireh that the customer will see on your website.', 'mijireh_checkout' ), 
+  				'default' => 'Pay securely with your credit card.'
+  			),
       );
     }
     
@@ -120,7 +160,6 @@ function woocommerce_mijireh_checkout_init() {
     public function process_payment($order_id) {
       global $woocommerce;
       
-      Mijireh::$access_key = $this->access_key;
       $mj_order = new Mijireh_Order();
       $wc_order = new WC_Order($order_id);
       
@@ -188,10 +227,46 @@ function woocommerce_mijireh_checkout_init() {
       
     }
     
-    public function receipt_page($order) {
-			echo '<p>'.__('Thank you for your order.', 'woocommerce').'</p>';
-		}
-		
+    public function add_page_slurp_meta() { 
+      if($this->is_slurp_page()) {
+        wp_enqueue_style('mijireh_css', $this->url . '/' . 'mijireh.css');
+        wp_enqueue_script('pusher', 'https://d3dy5gmtp8yhk7.cloudfront.net/1.11/pusher.min.js', null, false, true);
+        wp_enqueue_script('page_slurp', $this->url . '/' . 'page_slurp.js', array('jquery'), false, true);
+        
+        add_meta_box(  
+          'slurp_meta_box', // $id  
+          'Mijireh Page Slurp', // $title  
+          array(&$this, 'draw_page_slurp_meta_box'), // $callback  
+          'page', // $page  
+          'normal', // $context  
+          'high'); // $priority  
+      }
+    }
+
+    public function is_slurp_page() {
+      global $post;
+      $is_slurp = false;
+      if(isset($post) && is_object($post)) {
+        $content = $post->post_content;
+        if(strpos($content, '{{mj-checkout-form}}') !== false) {
+          $is_slurp = true;
+        }
+      }
+      return $is_slurp;
+    }
+
+    public function draw_page_slurp_meta_box($post) {
+      echo "<div id='mijireh_notice' class='mijireh-info alert-message info' data-alert='alert'>";
+      echo  "<div class='mijireh-logo'><img src='" . $this->url . "/images/mijireh-checkout-logo.png' alt='Mijireh Checkout Logo'></div>";
+      echo  "<div class='mijireh-blurb'>";
+      echo    "<h2>Slurp your custom checkout page!</h2>";
+      echo    "<p>Get the page designed just how you want and when you're ready, click the button below and slurp it right up.</p>";
+      echo    "<div id='slurp_progress' class='meter progress progress-info progress-striped active' style='display: none;'><div id='slurp_progress_bar' class='bar' style='width: 20%;'>Slurping...</div></div>";
+      echo    "<p class='aligncenter'><a href='#' id='page_slurp' rel=". $post->ID ." class='button-primary'>Slurp This Page!</a></p>";
+      echo    '<p class="aligncenter"><a class="nobold" href="' . Mijireh::preview_checkout_link() . '" id="view_slurp" target="_new">Preview Checkout Page</a></p>';
+      echo  "</div>";
+      echo  "</div>";
+    }
 	}
 	
 	/**
